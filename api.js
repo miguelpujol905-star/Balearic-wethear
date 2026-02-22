e61b6030fd526b2e48f566a92987fbb79b76ccf9
miguelpujol905-star <miguelpujol905@gmail.com>
2026-02-22 16:36:41 +0100

    Api cambios

--- api.js
+++ api.js
@@ -7,7 +7,7 @@ document.getElementById("Humedad-Palma").textContent = Math.round(data.main.humi
 })
 
 // API Menorca
-fetch("https://api.openweathermap.org/data/2.5/weather?lat=39.95&lon=4.11&appid=TU_API_KEY&units=metric&lang=es")
+fetch("https://api.openweathermap.org/data/2.5/weather?lat=39.95&lon=4.11&appid=61a8f88ebfe982ef3e5a18707129382e&units=metric&lang=es")
 .then(response => response.json())
 .then(data => {
   document.getElementById("Temperatura-Menorca").textContent = Math.round(data.main.temp);
@@ -15,11 +15,12 @@ fetch("https://api.openweathermap.org/data/2.5/weather?lat=39.95&lon=4.11&appid=
 });
 
 // API Ibiza
-fetch("https://api.openweathermap.org/data/2.5/weather?lat=38.98&lon=1.43&appid=TU_API_KEY&units=metric&lang=es")
+fetch("https://api.openweathermap.org/data/2.5/weather?lat=38.98&lon=1.43&appid=61a8f88ebfe982ef3e5a18707129382e&units=metric&lang=es")
 .then(response => response.json())
 .then(data => {
   document.getElementById("Temperatura-Ibiza").textContent = Math.round(data.main.temp);
   document.getElementById("Humedad-Ibiza").textContent = Math.round(data.main.humidity);
 });
+
 
 
